a424ea74df905ef2d2a5d6a8b19eb5a3584cd4dd
Takashi Kokubun <takashi-kokubun@cookpad.com>
2014-09-23 10:13:41 +0900

    Fix path

--- webapp/etc/env.sh
+++ webapp/etc/env.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export PATH=$HOME/.nodebrew/current/bin:$PATH
+export PATH="$PERL_PATH:$RUBY_PATH:$PYTHON_PATH:$NODE_PATH:/usr/local/go/bin:$PATH"
 # source ~/perl5/perlbrew/etc/bashrc
 # [[ -s "$HOME/.pythonbrew/etc/bashrc" ]] && source "$HOME/.pythonbrew/etc/bashrc"
 export PATH="$HOME/.rbenv/bin:$PATH"
